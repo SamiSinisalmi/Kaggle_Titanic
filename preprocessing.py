9f4e9ce48f62a3f7548c55cca19aeed8150e18d7
Sami Sinisalmi <sinisalmisami@gmail.com>
2020-07-30 17:37:52 +0300

    added numericing of data in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,5 @@
+from sklearn import preprocessing
+
 class Preprocessing():
     
     def __init__(self):
@@ -6,10 +8,9 @@ class Preprocessing():
         self.train = 0
         self.test = 0
         
-    def _name(self):
-        print('TODO')
+        self.encoder = preprocessing.LabelEncoder()
         
-    def _sex(self):
+    def _name(self):
         print('TODO')
         
     def _ticket(self):
@@ -18,12 +19,29 @@ class Preprocessing():
     def _cabin(self):
         print('TODO')
         
-    def _embark(self):
-        print('TODO')
+    def _fillnan_str(self):
+        # Replaces NaN values with 'Unknown'
+        column = ['Name', 'Sex', 'Ticket', 'Cabin', 'Embarked']
+        for name in column:
+            self.train[name].fillna('Unknown', inplace=True)
+            self.test[name].fillna('Unknown', inplace=True)
+            
+    def _fillnan_numeric(self):
+        # Replaces NaN values with 0
+        column = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+        for name in column:
+            self.train[name].fillna(0, inplace=True)
+            self.test[name].fillna(0, inplace=True)
         
-    def _fillna_with_zeros(self):
-        self.train.fillna(0, inplace=True)
-        self.test.fillna(0, inplace=True)
+    def _numerice_columns(self):
+        
+        self._fillnan_str()
+        self._fillnan_numeric()
+        
+        column = ['Sex', 'Name', 'Ticket', 'Cabin', 'Embarked']
+        for name in column:
+            self.train[name] = self.encoder.fit_transform(self.train[name])
+            self.test[name] = self.encoder.fit_transform(self.test[name])
 
     def preprocess(self, train, test):
         
@@ -32,9 +50,10 @@ class Preprocessing():
         self.train = train
         self.test = test
              
-        self._fillna_with_zeros()
+        self._numerice_columns()
         
         print('Preprocessing done')
+        print()
         
         return self.train, self.test
     
